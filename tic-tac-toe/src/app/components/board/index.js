1cf48847990cd039d4e52e6d71673a8f468f2831
Leonardo Romero <leonardoromero@Woloxs-MacBook-Pro-6.local>
2018-11-16 08:03:03 -0500

    Taking Turns

--- tic-tac-toe/src/app/components/board/index.js
+++ tic-tac-toe/src/app/components/board/index.js
@@ -6,14 +6,18 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
   heandClick = i => {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   };
 
   renderSquare = i => {
@@ -21,7 +25,7 @@ class Board extends React.Component {
   };
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
