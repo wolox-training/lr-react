ec71f09ab1ae945c9215d3f32eee6bca23c0552c
Leonardo Romero <leonardoromero@Woloxs-MacBook-Pro-6.local>
2018-11-16 11:45:57 -0500

    Showing the Past Moves

--- tic-tac-toe/src/app/components/game/index.js
+++ tic-tac-toe/src/app/components/game/index.js
@@ -40,6 +40,13 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = this.calculateWinner(current.squares);
+    const moves = history.map((step, move) => {
+      const desc = move ? 'Go to move #' + move : 'Go to game start';
+      return (
+        <li> 
+          <button onClick={() => this.jumpTo(move)}>{desc}</button> 
+        </li> );
+    });
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -54,7 +61,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
