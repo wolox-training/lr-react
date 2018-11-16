ef1e4dbc9d53b5889b93a84742aba81b88aad95d
Leonardo Romero <leonardoromero@Woloxs-MacBook-Pro-6.local>
2018-11-16 11:57:16 -0500

    Implementing Time Travel

--- tic-tac-toe/src/app/components/game/index.js
+++ tic-tac-toe/src/app/components/game/index.js
@@ -7,6 +7,7 @@ class Game extends React.Component {
     super(props);
     this.state = {
       history: [{ squares: Array(9).fill(null) }],
+      stepNumber: 0,
       xIsNext: true
     };
   }
@@ -23,7 +24,7 @@ class Game extends React.Component {
   };
 
   handleClick = i => {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (this.calculateWinner(squares) || squares[i]) {
@@ -32,13 +33,21 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{ squares: squares }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    });
+  };
+
+  stepNumber = step => {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
     });
   };
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = this.calculateWinner(current.squares);
     const moves = history.map((step, move) => {
       const desc = move ? 'Go to move #' + move : 'Go to game start';
