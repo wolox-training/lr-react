cd420a81f96834c5e75a32fb281581fef7b576b3
leonardofrp5 <leonardofrp5@gmail.com>
2019-04-08 14:12:26 -0500

    added some changed in structure

--- maquetado/javscript/ajax.js
+++ maquetado/javscript/ajax.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $.getJSON('http://demo8272131.mockable.io/', function(data) {
+  $.get('http://demo8272131.mockable.io/', function(data) {
       $.each( data, function( index, value ) {
         $('#msg').append('<tr>','<td class="items">' + value.id + '</td>', '<td class="items">' + value.name + '</td>', '<td class="items">' + value.description + '</td>','<tr>');
     });
