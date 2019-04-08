59a85e64aab249d10fa58935bfb5841bb582beb2
leonardofrp5 <leonardofrp5@gmail.com>
2019-04-08 09:36:02 -0500

    added some attributes of styles

--- maquetado/javscript/ajax.js
+++ maquetado/javscript/ajax.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $.getJSON('http://demo8272131.mockable.io/', function(data) {
       $.each( data, function( index, value ) {
-        $('#msg').append('<tr>','<td>' + value.id + '</td>', '<td>' +value.name + '</td>', '<td>' + "sss" + '</td>','<tr>');
+        $('#msg').append('<tr>','<td class="items">' + value.id + '</td>', '<td class="items">' + value.name + '</td>', '<td class="items">' + value.description + '</td>','<tr>');
     });
   });
 });
